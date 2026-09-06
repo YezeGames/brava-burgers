79140d6c3c117d96496e5ed2234e6eeab5102deb
YezeGames <mapachecito98@gmail.com>
2026-09-05 23:33:13 -0300

    Revert "Checkout desktop: modal ancho en 2 columnas para menos scroll"
    This reverts commit 2c39a249990ca7ac3bbb052cc948a6f0c6f2273f.

--- BravaBurgers/brava-shop.js
+++ BravaBurgers/brava-shop.js
@@ -170,16 +170,15 @@
 			},
 			afterShow: function (_instance, current) {
 				if (!current || !current.$content) return;
-				var isDesktop = window.innerWidth >= 769;
 				var css = {
-					maxWidth: isDesktop ? 'calc(100vw - 32px)' : 'calc(100vw - 16px)',
+					width: '350px',
+					maxWidth: 'calc(100vw - 16px)',
 					boxSizing: 'border-box',
 				};
 				if (isCheckout) {
-					css.width = isDesktop ? 'min(720px, calc(100vw - 32px))' : '350px';
 					css.padding = '0';
-					css.maxHeight = isDesktop ? '90vh' : '92vh';
-					css.height = isDesktop ? 'auto' : '92vh';
+					css.maxHeight = '92vh';
+					css.height = '92vh';
 					css.overflow = 'hidden';
 					css.display = 'flex';
 					css.flexDirection = 'column';
